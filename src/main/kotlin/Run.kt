152d6e92e909c867c322f47aea5ebfefed9f4a87
Arthur Embleton <arthur@blerg.net>
2016-12-01 09:30:00 +0000

    first commit

---
+++ src/main/kotlin/Run.kt
@@ -0,0 +1,85 @@
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import java.io.File
+/**
+ * Created by embletona on 30/11/2016.
+ */
+
+fun main(args: Array<String>) {
+    val talks = getTalks("talks.txt").map { it.first to it.second }.toMap()
+    printTracks(talks)
+}
+
+fun printTracks(talks:Map<String, Int>, track:Int=1) {
+
+    if (talks.isEmpty()) {
+        return
+    }
+
+    println()
+    println("# Track $track")
+    println()
+    val talksToPrint = createTrack(talks)
+    talksToPrint.forEach { println("${it.first} ${it.second}") }
+    println()
+    printTracks(talks.filterNot { talksToPrint.map { it.second }.contains(it.key) }, track + 1)
+}
+
+fun getTalks(fileName:String) = File(fileName).readLines().map {
+    val title = it.split(" ").dropLast(1).reduce { s1, s2 -> "$s1 $s2" }
+    val lengthOfTalk = it.split(" ").takeLast(1).first()
+    val time = if (lengthOfTalk == "lightning") {
+        5
+    } else {
+        lengthOfTalk.replace("min","").toInt()
+    }
+    Pair(title, time)
+}
+
+fun createTrack(talks:Map<String, Int>):List<Pair<String, String>> {
+    val morning = createSession(talks, 180, 180)
+    val afternoonTalks = talks.filterNot { morning.contains(it.key) }
+    val afternoon = createSession(afternoonTalks, 180, 240)
+
+    val morningStart = DateTime.now().withHourOfDay(9).withMinuteOfHour(0).withSecondOfMinute(0).withMillisOfSecond(0)
+    val decoratedMorning = decorateWithTimeStamps(talks, morning, morningStart).reversed()
+
+    val afternoonStart = DateTime.now().withHourOfDay(13).withMinuteOfHour(0).withSecondOfMinute(0).withMillisOfSecond(0)
+    val decoratedAfternoon = decorateWithTimeStamps(talks, afternoon, afternoonStart).reversed()
+
+    val lunch = listOf(Pair("12:00PM", "Lunch"))
+
+    val afternoonLength = talks.filter { afternoon.contains(it.key) }.values.sum()
+    val networkingStart = afternoonStart.plusMinutes(afternoonLength)
+    val fmt = DateTimeFormat.forPattern("KK:mmaa")
+    val timeStamp = fmt.print(networkingStart)
+    val networking = listOf(Pair(timeStamp, "Networking"))
+
+    return decoratedMorning + lunch + decoratedAfternoon + networking
+}
+
+tailrec fun createSession(talks:Map<String, Int>, minLength:Int, maxLength:Int, session:List<String> = emptyList(), timeUsed:Int = 0):List<String> {
+
+    val remainingTime = maxLength - timeUsed
+
+    if (talks.filter { it.value <= remainingTime }.isEmpty()) {
+        return session
+    }
+
+    val (talkToUse, lengthOfTalk) = talks.filter { it.value <= remainingTime }.entries.first()
+    val remainingTalks = talks.filterNot { it.key == talkToUse }
+    return createSession(remainingTalks, minLength, maxLength, session + talkToUse, timeUsed + lengthOfTalk )
+}
+
+tailrec fun decorateWithTimeStamps(allTalks:Map<String, Int>, talksToDecorate:List<String>, startTime:DateTime): List<Pair<String,String>> {
+    if (talksToDecorate.isEmpty()) {
+        return emptyList()
+    }
+
+    val talkName = talksToDecorate.first()
+    val talkTime = allTalks.get(talkName) ?: 0
+    val newStartTime = startTime.plusMinutes(talkTime)
+    val fmt = DateTimeFormat.forPattern("KK:mmaa")
+    val timeStamp = fmt.print(startTime)
+    return decorateWithTimeStamps(allTalks, talksToDecorate.drop(1), newStartTime) + Pair(timeStamp, talkName)
+}
